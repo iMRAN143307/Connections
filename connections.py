aa507f8483a686c782a37aed428158b0583b04e0
iMRAN143307 <imranmawji2@gmail.com>
2026-05-17 19:54:42 -0400

    adding more functionality

--- connections.py
+++ connections.py
@@ -21,8 +21,9 @@ all_foods = [
     [None, None, None, None, None],
     [None, None, None, None, None],
 ]
+main_cooking_area = [(440, 300), (840, 700)]
 foods_shown = all_foods[0]
-holding_item = None
+holding_item = False
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -32,9 +33,21 @@ while True:
             if event.button == 1:
                 mouse_pos = event_pos
                 """this is the left click handler, mouse pos is now a (x,y) tuple"""
+                if holding_item:
+                    if (
+                        mouse_pos[0] >= main_cooking_area[0][0]
+                        and mouse_pos[0] <= main_cooking_area[1][0]
+                    ):
+                        if (
+                            mouse_pos[1] >= main_cooking_area[0][1]
+                            and mouse_pos[1] <= main_cooking_area[1][1]
+                        ):
+                            print(
+                                "To implement: add sprites and put down a sprite in this case"
+                            )
                 if mouse_pos[1] < 237 and mouse_pos[1] > 35:
                     for box in food_positions:
-                        if mouse_pos[0] < box[1] + 201 and mouse_pos[0] > box[0] - 1:
+                        if mouse_pos[0] < box[1] + 201 and mouse_pos[0] >= box[0]:
                             holding_item = foods_shown[food_positions.index(box)]
 
     screen.fill((0, 0, 0))
